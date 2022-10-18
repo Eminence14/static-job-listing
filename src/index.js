2326e0fa5f4c59cf1ddaf81cd3025628f66046f0
Victor <victoribidunmoye@gmail.com>
2022-10-18 10:33:47 +0100

    filter almost completed, bugs abounding

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const getData = async () => {
   const response = await fetch("data.json");
   const data = await response.json();
   displayData(data);
-  return data
+  return data;
 };
 getData();
 
@@ -39,8 +39,8 @@ function dataTemp(datum) {
   </div>
   <hr>
   <ul class="card_skills">
-${datum.languages.map(dat => `<li class="skills">${dat}</li>`).join("")}
-${datum.tools.map(dat => `<li class="skills">${dat}</li>`).join("")}
+${datum.languages.map((dat) => `<li class="skills">${dat}</li>`).join("")}
+${datum.tools.map((dat) => `<li class="skills">${dat}</li>`).join("")}
   </ul>
 </div>
 
@@ -52,8 +52,8 @@ async function filter() {
   const skills = document.querySelectorAll(".skills");
   const allFilters = [];
 
-  skills.forEach(skill => {
-    skill.addEventListener("click", e => {
+  skills.forEach((skill) => {
+    skill.addEventListener("click", (e) => {
       let token = true;
       const targetText = e.target.textContent;
       const skillSet = document.querySelectorAll(".card_skills");
@@ -61,7 +61,7 @@ async function filter() {
 
       allFilters.push(targetText);
 
-      filterTexts.forEach(text => {
+      filterTexts.forEach((text) => {
         if (text.textContent.includes(e.target.textContent)) token = false;
       });
 
@@ -96,45 +96,54 @@ function showFilter(token, targetText) {
 
 clearFilters.addEventListener("click", () => {
   const searchParams = Array.from(filterContainer.children);
-  searchParams.forEach(param => {
+  searchParams.forEach((param) => {
     if (param.tagName.toLowerCase() === "div")
-      filterContainer.removeChild(param)
+      filterContainer.removeChild(param);
   });
-  clearFilter()
-  filter()
+  clearFilter();
+  filter();
 });
 async function clearFilter() {
-  const pageData = await getData()
-  displayData(pageData)
+  const pageData = await getData();
+  displayData(pageData);
 }
 
-filterContainer.addEventListener('click', e => {
-  if (!e.target.closest('span.remove')) return;
-  e.target.parentElement.remove()
+filterContainer.addEventListener("click", (e) => {
+  if (!e.target.closest("span.remove")) return;
+  e.target.parentElement.remove();
 
-  const allFilters = []
+  const allFilters = [];
   const appliedFilters = Array.from(filterContainer.children);
-  appliedFilters.splice(appliedFilters.length - 1, 1)
+  appliedFilters.splice(appliedFilters.length - 1, 1);
 
   // get index of the clicked filter
-  appliedFilters.forEach(filter => {
-    allFilters.push(Array.from(filter.children)[0].textContent)
-  })
+  appliedFilters.forEach((filter) => {
+    allFilters.push(Array.from(filter.children)[0].textContent);
+  });
 
-  clearFilter()
-  pageAfterRemFilter(allFilters)
-  filter()
-})
+  clearFilter();
+  pageAfterRemFilter(allFilters);
+  if (allFilters.length === 0) {
+    clearFilters.click()
+  }
+});
 
-// async function pageAfterRemFilter(allFilters) {
-//   await clearFilter()
-//   const allSkills = document.querySelectorAll('.card_skills')
+async function pageAfterRemFilter(allFilters) {
+  await clearFilter();
+  const allSkills = document.querySelectorAll(".card_skills");
 
-//   for (let key of allFilters) {
-//     allSkills.forEach(skill => {
-//       if (!skill.textContent.includes(key)) {
-//       }
-//     })
-//   }
-
-// }
+  for (let key of allFilters) {
+    allSkills.forEach((skill) => {
+      const skillParent = skill.parentElement;
+      if (skill.innerText.includes(key)) {
+        if (window.innerWidth > 800)
+          skillParent.style.display = 'flex'
+        else {
+          skillParent.style.display = 'block';
+        }
+      } else {
+        skillParent.style.display = 'none'
+      }
+    });
+  }
+}
